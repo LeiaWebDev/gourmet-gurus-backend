62735697847b47ed3b399eab3de321c53be60c82
Angelica Turbela <angelcturbela@gmail.com>
2023-08-22 15:56:39 +0200

    start routes

---
+++ routes/workshop.routes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+// const Workshop = require(...path)
+
+//GET ALL WORKSHOPS
+
+router.get("/", async (req, res, next) => {
+    try {
+        const allWorkShops = await Workshop.find()
+
+    }catch (error) {
+        console.log(error);
+    }
+})
+
+
+// Get one workshop
+router.get(/)
+
+//POST
+
+
+
+//UPDATE
+
+
+//DELETE
+
+
+
+module.exports = router;
