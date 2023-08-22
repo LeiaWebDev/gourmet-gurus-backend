71d78b069550dae4a2cd3db29dcb0d773f8695e2
LeiaWebDev <leia@cowmeuh.net>
2023-08-22 18:53:31 +0200

    users routes updated

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,6 +1,73 @@
 const express = require("express");
 const router = express.Router();
 
+const User = require("../models/User.model")
+const Workshop = require("../models/Workshop.model")
+const Booking = require("../models/Booking.model")
+
+
+// all routes are prefixed with /api
+
+
+// route to get all users, 
+router.get("/users", async(req, res, next)=>{
+    console.log("View all the users")
+    try {
+        const allUsers = await User.find()
+        res.json(allUsers)
+    } catch (error) {
+        next(error)
+    }
+})
+
+// route to get all teachers
+router.get("/teachers", async(req, res, next)=>{
+    console.log("View all teachers")
+    try {
+        const allTeachers = await User.find({role:"Teacher"})
+        res.json(allTeachers)
+    } catch (error) {
+        next(error)
+    }
+})
+
+// route to get one user
+router.get("/users/:userId", async(req, res, next)=>{
+    
+    try {
+        const oneUser = await User.findById(req.params.userId)
+        res.json(oneUser)
+    } catch (error) {
+        next(error)
+    }
+})
+
+// route to get one teacher
+router.get("/teachers/:userId", async(req, res, next)=>{
+    
+    try {
+        const oneTeacher = await allTeachers.findById(req.params.userId)
+        res.json(oneTeacher)
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+// delete a teacher with specifid Id
+router.get("/teachers/:userId", async(req, res, next)=>{
+    const id = req.params.userId
+    try {
+        await allTeachers.findByIdAndDelete(id)
+        res.json({message: `teacher ${id} was deleted`})
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+
+
 
 
 module.exports = router;
