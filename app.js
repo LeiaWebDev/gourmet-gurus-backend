b461dfbe114b90892fc9672387640565d74f267b
LeiaWebDev <leia@cowmeuh.net>
2023-08-23 12:56:26 +0200

    updated authentication, authroutes, error handling

--- app.js
+++ app.js
@@ -2,26 +2,93 @@
 // https://www.npmjs.com/package/dotenv
 require("dotenv").config();
 
+
+// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
+require("./config");
+
 // ℹ️ Connects to the database
 require("./db");
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
+const cors = require("cors")
 const express = require("express");
+const morgan = require("morgan")
+const cookieParser = require("cookie-parser")
+const helmet = require("helmet")
+const rateLimiter = require("express-rate-limit")
+const jwt = require("jsonwebtoken")
+const { errorHandler, notFound } = require("./error-handling/index")
+const User = require("./models/User.model")
+const PORT = 5005
 
+
+
+
+// INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-require("./config")(app);
+// additional protection of app
+// Setting limiter
+const limiter = rateLimiter({
+	windowMs: 1000,
+	max: 100,
+	standardHeaders: true,
+})
+
+
+
+// MIDDLEWARES
+
+app.use(
+	cors({
+		origin: [
+			"http://localhost:5174",
+			"http://127.0.0.1:5174",
+			process.env.ORIGIN,
+		],
+	})
+)
+
+// Helmet, simple, efficient, wear your helmet.
+app.use(helmet())
+app.use(limiter)
+app.use(express.json())
+app.use(morgan("dev"))
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
 
 // 👇 Start handling routes here
+
+// ROUTES - https://expressjs.com/en/starter/basic-routing.html
+// app.get("/docs", (req, res) => {
+// 	res.sendFile(__dirname + "/views/docs.html")
+// })
+
+// Nested routing
+// Any request starting by /api will go in the index route.
+
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
+
+// Import the custom error handling middleware
+const {errorHandler, notFoundHandler} = require("./error-handling")
+
+//Set up custom error handling middleware
+app.use(errorHandler)
+app.use(notFoundHandler)
+
+
+
+// START SERVER
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`)
+})
 
 module.exports = app;
--- error-handling/index.js
+++ error-handling/index.js
@@ -1,19 +1,49 @@
-module.exports = (app) => {
-  app.use((req, res, next) => {
-    // this middleware runs whenever requested page is not available
-    res.status(404).json({ message: "This route does not exist" });
-  });
 
-  app.use((err, req, res, next) => {
-    // whenever you call next(err), this middleware will handle the error
-    // always logs the error
-    console.error("ERROR", req.method, req.path, err);
 
-    // only render if the error ocurred before sending the response
-    if (!res.headersSent) {
-      res.status(500).json({
-        message: "Internal server error. Check the server console",
-      });
-    }
-  });
+function errorHandler (error, req, res, next) {
+  // This middleware has 4 arguments. It will run whenever `next(error)` is called.
+
+  // Log the error first
+  console.error("ERROR", req.method, req.path, error);
+let status = 500
+if (error.name === "ValidationError") {
+  status = 400
+}
+  // Check if the response was already sent, as sending a response twice for the same request will cause an error.
+  if (!res.headersSent) {
+
+    // If not, send a response with status code 500 and a generic error message
+    res
+      .status(status)
+      .json({ message: error.message, error: error.name});
+  }
 };
+
+function notFoundHandler (req, res, next) {
+  // This middleware will run whenever the requested route is not found
+  res
+    .status(404)
+    .json({ message: "This route does not exist" });
+};
+
+
+module.exports = {errorHandler, notFoundHandler}
+// module.exports = (app) => {
+  // app.use((req, res, next) => {
+  //   // this middleware runs whenever requested page is not available
+  //   res.status(404).json({ message: "This route does not exist" });
+  // });
+
+  // app.use((err, req, res, next) => {
+  //   // whenever you call next(err), this middleware will handle the error
+  //   // always logs the error
+  //   console.error("ERROR", req.method, req.path, err);
+
+    // // only render if the error ocurred before sending the response
+    // if (!res.headersSent) {
+    //   res.status(500).json({
+    //     message: "Internal server error. Check the server console",
+    //   });
+//     }
+//   });
+// };
