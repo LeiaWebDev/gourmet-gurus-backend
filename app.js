3803b2b1823b102ab4cd2e83cc4e575dad4af33a
LeiaWebDev <leia@cowmeuh.net>
2023-08-25 15:15:39 +0200

    changed frontend back port to 5173, to match with backend

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ const limiter = rateLimiter({
 app.use(
   cors({
     origin: [
-      "http://localhost:5174",
-      "http://127.0.0.1:5174",
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
       process.env.ORIGIN,
     ],
   })
